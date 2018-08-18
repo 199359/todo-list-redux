af3a618ee2ea3af9a2e8f7b45f2627d3ba666f83
Robert Piotrowski <robertpiotrowski93@gmail.com>
2018-08-18 18:23:19 +0200

    file to tasklist component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import TaskList from './components/TaskList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          App
+          <TaskList />
       </div>
     );
   }
---
+++ src/components/TaskList/TaskList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TaskList = () => (
+    <div>
+        <p>TaskList</p>
+    </div>
+)
+
+export default TaskList
