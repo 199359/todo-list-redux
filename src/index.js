1844fae560dd42d5610d6f5e8875ee4c590481a2
Robert Piotrowski <robertpiotrowski93@gmail.com>
2018-08-18 18:37:26 +0200

    material-ui installed, store and reducer for task list

--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
+import { createStore, combineReducers } from 'redux'
+import taskListReducer from './reducer/taskListReducer'
+
+const rootReducer = combineReducers({
+    tasklist: taskListReducer
+})
+
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
-<App />,
- document.getElementById('root'));
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
---
+++ src/reducer/taskListReducer.js
@@ -0,0 +1,9 @@
+const initialStore = {
+    tasks: []
+}
+
+const reducer = (state = initialStore, action) => {
+    return state
+}
+
+export default reducer
